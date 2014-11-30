68cca4bde4c3f0089e8616b0ca4a15fb82ce7f6b
Shiyun Huang <shiyunh2011@gmail.com>
2014-11-29 17:43:26 -0800

    updating Replica class

--- src/database/Replica.java
+++ src/database/Replica.java
@@ -1,16 +1,37 @@
 package database;
 
-public class Replica {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Set;
+
+public class Replica extends UnicastRemoteObject{
 	String RMIRegistryAddress;
 	boolean isLeader;
-	Integer startOfKeyRange;
-	Integer endOfKeyRange;
+	int startOfKeyRange;
+	int endOfKeyRange;
+	String name;
 	
+	Log dataLog;
+	Set<Replica> leaderSet;
 	
-	public static void main(String[] args) {
-		
-		
+	public Replica(String RMIRegistryAddress, boolean isLeader, int startOfKeyRange, int endOfKeyRange, String name) throws RemoteException {
+		super();
+		this.RMIRegistryAddress = RMIRegistryAddress;
+		this.isLeader = isLeader;
+		this.startOfKeyRange = startOfKeyRange;
+		this.endOfKeyRange = endOfKeyRange;
+		this.name = name;
 		
 	}
+	
+	public void setLog(Log dataLog) {
+		this.dataLog = dataLog;
+	}
+	
+	public void setLeaderSet(Set<Replica> leaderSet) {
+		this.leaderSet = leaderSet;
+	}
+	
+	
 	
 }
