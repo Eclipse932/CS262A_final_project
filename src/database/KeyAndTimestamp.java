ece4b34c609c64362883edb8bb0b1f167e4a388b
mew2ub <matt.weber@berkeley.edu>
2014-12-13 14:47:27 -0800

    added KeyAndValue

---
+++ src/database/KeyAndTimestamp.java
@@ -0,0 +1,35 @@
+package database;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class KeyAndTimestamp implements Serializable {
+
+	/**
+	 * Default serialization id
+	 */
+	private static final long serialVersionUID = 1L;
+	Integer key;
+	Instant timestamp;
+	
+
+	
+	public KeyAndTimestamp(Integer key, Instant timestamp) {
+		super();
+		this.key = key;
+		this.timestamp = timestamp;
+	}
+	public Integer getKey() {
+		return key;
+	}
+	public void setKey(Integer Key) {
+		this.key = key;
+	}
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+}
